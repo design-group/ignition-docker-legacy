470b0e1351682e5160b268c71c185592079c83cd
keith-gamble <keith.gamble@bwdesigngroup.com>
2022-10-25 23:29:12 -0700

    Updated build commands

--- build-and-push.sh
+++ build-and-push.sh
@@ -7,7 +7,7 @@ docker build -t bwdesigngroup/ignition-docker-iiot:8.1.21 -f secondary-versions/
 docker build -t bwdesigngroup/ignition-docker-mes:8.1.20 -f secondary-versions/mes --build-arg IGNITION_VERSION=8.1.20 secondary-versions/mes; docker push bwdesigngroup/ignition-docker-mes:8.1.20
 
 # Build out all pre-8.1.17 Versions and push them to dockerhub
-for i in $(seq 13 16); do docker build -t bwdesigngroup/ignition-docker:8.1.$i -f secondary-versions/pre-8-1-17-Dockerfile --build-arg IGNITION_VERSION=8.1.$i .; docker push bwdesigngroup/ignition-docker:8.1.$i; done
+for i in $(seq 13 16); do docker build -t bwdesigngroup/ignition-docker:8.1.$i -f secondary-versions/pre-8-1-17 --build-arg IGNITION_VERSION=8.1.$i .; docker push bwdesigngroup/ignition-docker:8.1.$i; done
 
 # Build out all current versions and push them to dockerhub
 for i in $(seq 17 21); do docker build -t bwdesigngroup/ignition-docker:8.1.$i --build-arg IGNITION_VERSION=8.1.$i .; docker push bwdesigngroup/ignition-docker:8.1.$i; done
